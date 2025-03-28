ddb3380847c9c1a13c0395bf97198dbe236bf417
andre <andre.rossell@icloud.com>
2025-03-28 17:19:20 -0600

    floats segundo test

--- clite.py
+++ clite.py
@@ -3,10 +3,10 @@ import ply.lex as lex
 tokens = [ 'INT' , 'FLOAT']
 
 t_ignore  = ' \t'
-digit = r'[0-9]'
-expNotation = r'[eE][-+]?[0-9]+'
+digit = r'([0-9])'
+expNotation = r'([eE][-+]?[0-9]+)'
 
-floating = fr'{digit}*\.{digit}*'
+floating = fr'({digit}+{expNotation})|({digit}*\.{digit}*{expNotation}?)'
 @lex.TOKEN(floating)
 def t_FLOAT(t):
   t.value = float(t.value)
