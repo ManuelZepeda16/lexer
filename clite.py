f16ae2680842401e229ecd1824a932efcd7e8aad
Andre Rossell Manrique <andre@Andres-MacBook-Pro.local>
2025-03-28 13:07:46 -0600

    floats: primera prueba

--- clite.py
+++ clite.py
@@ -1,9 +1,14 @@
 import ply.lex as lex
 
-tokens = [ 'INT' ]
+tokens = [ 'INT' , 'FLOAT']
 
 t_ignore  = ' \t'
 
+def t_FLOAT(t):
+  r'[0-9]*\.[0-9]*'
+  t.value = float(t.value)
+  return t
+  
 def t_INT(t):
   r'[0-9]+(_[0-9]+)*'
   t.value = int(t.value.replace("_", ""))
